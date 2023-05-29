aff043a98610cd4be42e2951a0e91f9f053f243b
yang-road-001 <236548116@qq.com>
2023-05-29 16:17:33 +0800

    test

--- day91_05_29/test.cc
+++ day91_05_29/test.cc
@@ -3,7 +3,13 @@
 using namespace std;
 int main(int argc, char const *argv[]) 
 {
-	
+	int number = 10;
+	const char * pstr = "hello,world";
+
+    printf("hello,world %d%s\n", number, pstr);
+
+    LogError("hello, world, %d,%p %s %d\n",
+             number, &number, pstr, number);
 
 	return 0;
 } 
